6132f8418dc2115a839efe1b562430d4ef7bda26
Milton <miltoncoronel2004@gmail.com>
2024-02-02 13:04:31 -0300

    Cambios logicos

--- procesar.php
+++ procesar.php
@@ -2,41 +2,48 @@
 session_start();
 if(isset($_POST["submit"]) && isset($_POST["cliente"]) && isset($_POST["habitacion"]) && isset($_POST["fechaEntrada"]) && isset($_POST["fechaSalida"])) {
 
-  include("Models/reserva_habitaciones.php");
+  if(!empty($_POST["cliente"]) && !empty($_POST["habitacion"]) && !empty($_POST["fechaEntrada"]) && !empty($_POST["fechaEntrada"])) {
+
+    include("Models/reserva_habitaciones.php");
 
 
-  $cliente = htmlspecialchars($_POST["cliente"]);
-  $habitacion = $_POST["habitacion"];
-  $fechaEntrada = $_POST["fechaEntrada"];
-  $fechaSalida = $_POST["fechaSalida"];
-
-  $validarExistencia = (new Habitacion($habitacion))->validarExistencia($habitacion);
-
-
-  if(!$validarExistencia) {
-    $_SESSION['mensaje'] = "Habitación inexistente.";
-    header("Location: index.php");
-  } else {
-    $reserva = new Reserva($cliente, $habitacion, $fechaEntrada, $fechaSalida);
-
-    if ($reserva->validarDisponibilidad($habitacion, $fechaEntrada, $fechaSalida)) {
-
-      $_SESSION['mensaje'] = "Fechas invalidas o no disponibles.";
-      header("Location: index.php");
-
-    } else {
-        if($reserva->crearReserva($cliente, $habitacion, $fechaEntrada, $fechaSalida)) {
-          
-          $_SESSION['mensaje'] = "Se ha reservado exitosamente.";
-          header("Location: index.php");
-
-        } else {
-          $_SESSION["mensaje"] = "Error al crear la reserva";
-        }
-    }
-
-  }
+    $cliente = htmlspecialchars($_POST["cliente"]);
+    $habitacion = $_POST["habitacion"];
+    $fechaEntrada = $_POST["fechaEntrada"];
+    $fechaSalida = $_POST["fechaSalida"];
   
+    $validarExistencia = (new Habitacion($habitacion))->validarExistencia($habitacion);
+  
+  
+    if(!$validarExistencia) {
+      $_SESSION['mensaje'] = "Habitación inexistente.";
+      header("Location: index.php");
+    } else {
+      $reserva = new Reserva($cliente, $habitacion, $fechaEntrada, $fechaSalida);
+  
+      if ($reserva->validarDisponibilidad($habitacion, $fechaEntrada, $fechaSalida)) {
+  
+        $_SESSION['mensaje'] = "Fechas invalidas o no disponibles.";
+        header("Location: index.php");
+  
+      } else {
+          if($reserva->crearReserva($cliente, $habitacion, $fechaEntrada, $fechaSalida)) {
+            
+            $_SESSION['mensaje'] = "Se ha reservado exitosamente.";
+            header("Location: index.php");
+  
+          } else {
+            $_SESSION["mensaje"] = "Error al crear la reserva";
+          }
+      }
+  
+    }
+    
+  } else {
+    $_SESSION['mensaje'] = "Complete los datos para continuar.";
+    header("Location: index.php");
+  }
+
   
 } else {
   $_SESSION['mensaje'] = "Complete los datos para continuar.";
